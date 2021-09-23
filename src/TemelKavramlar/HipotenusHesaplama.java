763cd971e83fb6969fe63378ad43a2b33314eb51
Emre <emreedemir@yahoo.com>
2021-09-23 19:54:47 +0300

    Java101-18 HipotenusHesaplama

---
+++ src/TemelKavramlar/HipotenusHesaplama.java
@@ -0,0 +1,17 @@
+package TemelKavramlar;
+import java.util.Scanner;
+
+public class HipotenusHesaplama
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int kısaKenar,uzunKenar; double hipotenus;
+        System.out.print("Kısa Kenarı Giriniz: ");
+        kısaKenar = input.nextInt();
+        System.out.print("Uzun Kenarı Giriniz: ");
+        uzunKenar = input.nextInt();
+        hipotenus = Math.sqrt((kısaKenar*kısaKenar)+(uzunKenar*uzunKenar));
+        System.out.println("Hippotenus: "+hipotenus);
+    }
+}
