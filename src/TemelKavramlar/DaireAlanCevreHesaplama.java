36d00ed24e6b5257c8e9d3e1d1d86b60c0e791d2
Emre <emreedemir@yahoo.com>
2021-09-24 13:04:47 +0300

    Java101-19 DaireAlanCevreHesaplama

---
+++ src/TemelKavramlar/DaireAlanCevreHesaplama.java
@@ -0,0 +1,21 @@
+package TemelKavramlar;
+import java.util.Scanner;
+public class DaireAlanCevreHesaplama
+{
+    public static void main(String[] args)
+    {
+        int yariCap;
+        double pi = 3.1415, alan, cevre;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yarıçapı Giriniz: ");
+        yariCap = input.nextInt();
+
+
+        alan = pi*yariCap*yariCap;
+        cevre = 2*pi*yariCap;
+        System.out.println("Dairenin Alanı: "+alan);
+        System.out.println("Dairenin Çevresi: "+cevre);
+
+
+    }
+}
