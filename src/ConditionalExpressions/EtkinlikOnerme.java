7c58117338c73e1bda33004a18df9264fdd9f936
Emre <emreedemir@yahoo.com>
2021-09-25 11:52:27 +0300

    Java101-29 EtkinlikOnerme

---
+++ src/ConditionalExpressions/EtkinlikOnerme.java
@@ -0,0 +1,35 @@
+package ConditionalExpressions;
+import java.util.Scanner;
+
+public class EtkinlikOnerme
+{
+    public static void main(String[] args)
+    {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık Giriniz: ");
+        heat = input.nextInt();
+
+        if (heat<5)
+        {
+            System.out.println("Kayak yapabilirsiniz ");
+        }
+        else if(heat>=5 && heat<=25)
+        {
+            if (heat<=15)
+            {
+                System.out.println("Sinemaya gidebilirsiniz");
+            }
+            if (heat>=10)
+            {
+                System.out.println("Pikniğe gidebilirsiniz");
+            }
+            else
+            {
+                System.out.println("Yüzmeye gidebilirsiniz");
+            }
+
+        }
+    }
+}
