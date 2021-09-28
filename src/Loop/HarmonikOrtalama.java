9e8c3d4db444eb34c53898593d8e3788b3d45389
Emre <emreedemir@yahoo.com>
2021-09-28 17:33:22 +0300

    Java101-44 HarmonikOrtalama

---
+++ src/Loop/HarmonikOrtalama.java
@@ -0,0 +1,18 @@
+package Loop;
+import java.util.Scanner;
+
+public class HarmonikOrtalama
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("N sayısını girin: ");
+        int n = input.nextInt();
+        double result =0;
+        for (double i =1;i<=n;i++)
+        {
+            result +=(1/i);
+        }
+        System.out.println("Sonuç: "+result);
+    }
+}
