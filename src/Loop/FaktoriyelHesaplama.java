8c54a575854efd09c964ebb34f35a153023e95de
Emre <emreedemir@yahoo.com>
2021-09-28 14:50:34 +0300

    Java101-38 FaktoriyelHesaplama

---
+++ src/Loop/FaktoriyelHesaplama.java
@@ -0,0 +1,19 @@
+package Loop;
+import java.util.Scanner;
+public class FaktoriyelHesaplama
+{
+    public static void main(String[] args)
+    {
+        int sayi,faktoriyel=1;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayi Giriniz: ");
+        sayi = input.nextInt();
+
+        for (int i =1;i<=sayi;i++)
+        {
+            faktoriyel *= i;
+        }
+        System.out.println("Faktöriyel: " +faktoriyel);
+    }
+}
