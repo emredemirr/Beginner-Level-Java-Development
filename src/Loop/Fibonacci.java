9b12848e7dc27c0b2368f415195c33573c6456db
Emre <emreedemir@yahoo.com>
2021-09-29 17:11:29 +0300

    Java101-Odev Fibonacci Hesaplama

---
+++ src/Loop/Fibonacci.java
@@ -0,0 +1,24 @@
+package Loop;
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static void main(String[] args)
+    {
+        int n,fibonacci=0,a=0,b=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman Sayısını Giriniz: ");
+        n = input.nextInt();
+
+
+        System.out.println(fibonacci);
+        System.out.println(fibonacci+1);
+        for (int i =1;i<n;i++)
+        {
+            fibonacci = a + b;
+            System.out.println(fibonacci);
+            a=b;
+            b=fibonacci;
+        }
+    }
+}
---
+++ src/Loop/TersUcgen.java
@@ -0,0 +1,14 @@
+package Loop;
+import java.util.Scanner;
+
+public class TersUcgen
+{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kaç Basamaklı olsun: ");
+        n = input.nextInt();
+
+    }
+}
