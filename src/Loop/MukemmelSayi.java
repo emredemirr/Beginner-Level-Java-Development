97e852c46bad0f6e4df617273f8430c806d3acf3
Emre Demir <emreedemir@yahoo.com>
2022-10-10 13:24:11 +0300

    Java101-Odev Mükemmel Sayı

---
+++ src/Loop/MukemmelSayi.java
@@ -0,0 +1,29 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class MukemmelSayi
+{
+    public static void main(String[] args)
+    {
+        int number,total=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı Girin: ");
+        number = input.nextInt();
+
+        for (int i=1; i<number;i++)
+        {
+            if (number%i == 0)
+            {
+                total += i;
+            }
+        }
+        System.out.println(total);
+
+        if (total==number)
+            System.out.println(number+" Mükemmel Sayıdır.");
+        else
+            System.out.println(number+" Mükemmel Sayı Değildir.");
+
+    }
+}
