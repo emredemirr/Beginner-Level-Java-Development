2c60d31b984475e912d29579d958ebd8b70ef78a
Emre Demir <emreedemir@yahoo.com>
2022-10-10 12:45:35 +0300

    Java101-Odev Ters Üçgen

--- src/Loop/TersUcgen.java
+++ src/Loop/TersUcgen.java
@@ -5,10 +5,26 @@ public class TersUcgen
 {
     public static void main(String[] args)
     {
-        int n;
+        int number,spaceLoop=1,starLoop;
+        char star ='*', space =' ';
         Scanner input = new Scanner(System.in);
-        System.out.println("Kaç Basamaklı olsun: ");
-        n = input.nextInt();
+        System.out.print("Kaç Basamaklı olsun: ");
+        number = input.nextInt();
+        starLoop=number;
+
+        for(int i =0;i<number;i++) {
+            for (int j =0; j<spaceLoop;j++) {
+                System.out.print(space);
+            }
+            spaceLoop++;
+
+            for (int k=1;k<(starLoop*2);k++) {
+                System.out.print(star);
+            }
+            starLoop=starLoop-1;
+
+            System.out.print("\n");
+        }
 
     }
 }
