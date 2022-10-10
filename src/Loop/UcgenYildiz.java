acdf43a78a87330c43bd3692bf236bb5186af4bb
Emre Demir <emreedemir@yahoo.com>
2022-10-10 12:44:14 +0300

    Java101 - Yıldızlar ile Elmas Oluşturma

--- src/Loop/UcgenYildiz.java
+++ src/Loop/UcgenYildiz.java
@@ -5,22 +5,44 @@ public class UcgenYildiz
 {
     public static void main(String[] args)
     {
-        int n;
+        int number,spaceLoop=1,starLoop;
+        char star ='*', space =' ';
         Scanner input = new Scanner(System.in);
         System.out.println("N değerini girin: ");
-        n = input.nextInt();
+        number = input.nextInt();
+        starLoop=number;
 
-        for (int i =1; i<=n;i++)
+        for (int i =1; i<=number;i++)
         {
-            for (int j=1;j<=(n-i);j++)
+            if(i==number)
+                break;
+
+            for (int j=1;j<=(number-i);j++)
             {
-                System.out.print(" ");
+                System.out.print(space);
             }
             for (int k =1;k<=(2*i)-1;k++)
             {
-                System.out.print("x");
+                System.out.print(star);
             }
-            System.out.println(" ");
+            System.out.println(space);
+        }
+
+        for(int z =0;z<number;z++)
+        {
+            for (int y =1; y<spaceLoop;y++)
+            {
+                System.out.print(space);
+            }
+            spaceLoop++;
+
+            for (int x=1;x<(starLoop*2);x++)
+            {
+                System.out.print(star);
+            }
+            System.out.println(space);
+            starLoop=starLoop-1;
+
         }
     }
 }
--- src/TemelKavramlar/KdvHesaplama.java
+++ src/TemelKavramlar/KdvHesaplama.java
@@ -6,13 +6,13 @@ public class KdvHesaplama
 {
     public static void main(String[] args)
     {
-        float tutar,kdvOrani,Fiyat;
+        float tutar,kdvOrani,fiyat;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Ürün Tutarını Giriniz: ");
         tutar = input.nextFloat();
         kdvOrani = (tutar/100)*18;
-        Fiyat = tutar+kdvOrani;
-        System.out.println("Ürün Fiyatı: "+ Fiyat+"TL");
+        fiyat = tutar+kdvOrani;
+        System.out.println("Ürün Fiyatı: "+ fiyat+"TL");
     }
 }
