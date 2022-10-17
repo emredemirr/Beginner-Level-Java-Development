185d866000fae98513a4f0ff7b709071f7746d7e
Emre Demir <emreedemir@yahoo.com>
2022-10-17 23:35:17 +0300

    Java101 Matrisin Transpozunu Bulma

---
+++ src/Arrays/TransposeMatrix.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class TransposeMatrix
+{
+    public static void main(String[] args)
+    {
+
+        int[][] arr = { {1,2,3} , {4,5,6} };
+
+        for (int i = 0; i < arr.length;i++)
+        {
+            for (int j = 0; j < arr[i].length;j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("------");
+
+        for (int j = 0; j < arr[0].length;j++)
+        {
+            for (int i = 0;i < arr.length;i++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
