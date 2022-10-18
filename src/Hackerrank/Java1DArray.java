4b42b07755e47dd1d99c172b280a1a705e32bab5
Emre Demir <emreedemir@yahoo.com>
2022-10-18 17:50:54 +0300

    Java101 Hackerrank 1D Array

---
+++ src/Hackerrank/Java1DArray.java
@@ -0,0 +1,22 @@
+package Hackerrank;
+import java.util.Scanner;
+
+public class Java1DArray
+{
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i =0;i<n; i++)
+        {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
