6aceb1fccd9e8ae541f5f33b19bb4c5394f96e70
Emre Demir <emreedemir@yahoo.com>
2022-10-22 23:42:41 +0300

    Java101 Hackerrank Int to String Solution

---
+++ src/Hackerrank/JavaIntToString.java
@@ -0,0 +1,63 @@
+/*
+ *  Link       : https://www.hackerrank.com/challenges/java-int-to-string
+ *  @author    : Emre Demir
+ *  Repository : https://github.com/emredemirr/Java101
+ *  Hackerrank : https://www.hackerrank.com/emreedemir
+ */
+
+package Hackerrank;
+import java.security.Permission;
+import java.util.Scanner;
+
+public class JavaIntToString
+{
+    public static void main(String[] args)
+    {
+
+        DoNotTerminate.forbidExit();
+
+        try
+        {
+            Scanner in = new Scanner(System.in);
+            int n = in .nextInt();
+            in.close();
+            String s = Integer.toString(n);
+
+            if (n == Integer.parseInt(s))
+                System.out.println("Good job");
+
+            else
+                System.out.println("Wrong answer.");
+        }
+        catch (DoNotTerminate.ExitTrappedException e)
+        {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+
+}
+
+class DoNotTerminate
+{
+
+    public static class ExitTrappedException extends SecurityException
+    {
+        private static final long serialVersionUID = 1;
+    }
+    public static void forbidExit()
+    {
+        final SecurityManager securityManager = new SecurityManager()
+        {
+            @Override
+            public void checkPermission(Permission permission)
+            {
+                if (permission.getName().contains("exitVM"))
+                {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
+
